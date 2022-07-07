0cf7ac2a2b9c4bdf1499498abb8ebea798b59c63
Srinath N.S <srinath1@users.noreply.github.com>
2022-07-07 23:04:45 +0200

    Update courseCard.js

--- components/cards/courseCard.js
+++ components/cards/courseCard.js
@@ -1,5 +1,5 @@
 import {Card,Badge} from 'antd'
-import Link from 'next/Link'
+import Link from 'next/link'
 import{ currencyFormatter }from '../../utils/helpers'
 const {Meta}=Card
 const CourseCard=({course})=>{
